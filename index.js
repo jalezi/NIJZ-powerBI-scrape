50c5906d00c0e009f664a2216054a106421228a7
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-25 17:28:31 +0100

    refactor: +getTimestamp, +getVacs

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 import puppeteer from 'puppeteer';
 
+const getTimestamp = titles => {
+  const date = titles
+    .filter(node => {
+      const date = node.length === 1 && node[0].split(' ');
+      return date && date.length === 4;
+    })[0][0]
+    .split(' ')
+    .map(item => item.replace('.', ''));
+
+  const time = date[3].split(':');
+  const dateForUTC = [date[2], date[1] - 1, date[0], ...time];
+  return Date.UTC(...dateForUTC);
+};
+
+const getVacs = titles =>
+  titles
+    .filter(node => node.length === 2 && node[1].endsWith('odmerek'))
+    .reduce((acc, [value, key]) => {
+      key.includes('1') &&
+        (acc.administered = { toDate: +value.replace('.', '') });
+      key.includes('2') &&
+        (acc.administered2nd = { toDate: +value.replace('.', '') });
+      return acc;
+    }, {});
+
 const scrap_NIJZ_powerBI = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -13,29 +38,8 @@ const scrap_NIJZ_powerBI = async () => {
       await svg.$$eval('title', nodes => nodes.map(node => node.innerHTML))
   );
   const titlesResolved = await Promise.all(text);
-  const date = titlesResolved
-    .filter(node => node.length === 1)
-    .filter(item => {
-      const date = item[0].split(' ');
-      return date.length === 4;
-    })
-    .map(item => item[0])[0]
-    .split(' ')
-    .map(item => item.replace('.', ''));
-
-  const time = date[3].split(':');
-  const dateForUTC = [date[2], date[1] - 1, date[0], ...time];
-  const timestamp = Date.UTC(...dateForUTC);
-
-  const vacs = titlesResolved
-    .filter(node => node.length === 2 && node[1].endsWith('odmerek'))
-    .reduce((acc, [value, key]) => {
-      key.includes('1') &&
-        (acc.administered = { toDate: +value.replace('.', '') });
-      key.includes('2') &&
-        (acc.administered2nd = { toDate: +value.replace('.', '') });
-      return acc;
-    }, {});
+  const timestamp = getTimestamp(titlesResolved);
+  const vacs = getVacs(titlesResolved);
 
   // const allVisualModern = await page.$$('visual-modern');
   // console.log(allVisualModern.length);
