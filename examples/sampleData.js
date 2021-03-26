6a5977177e4aed2d0b3414c8aa8d880de48838bb
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-26 19:11:44 +0100

    feat(examples): +sampleData
    represents item from parsed csv file

---
+++ examples/sampleData.js
@@ -0,0 +1,8 @@
+export default {
+  date: '2021-03-24',
+  'vaccination.administered': '7481',
+  'vaccination.administered.todate': '198698',
+  'vaccination.administered2nd': '2533',
+  'vaccination.administered2nd.todate': '107096',
+  'vaccination.used.todate': '',
+};
