f3c297b744fe92a7000c19ed465b620950863388
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-27 02:53:04 +0100

    feat(src): export GetDose
    nijz-dash-fetch as GetDose

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as scrap_NIJZ_powerBI } from './scrap_NIJZ_powerBI.js';
 export { default as writeCSV } from './writeCSV.js';
 export { default as readCSV } from './readCSV.js';
+export { default as GetDose } from './nijz-dash-fetch.js';
