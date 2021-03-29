07be4307af8139b77cf06128b4481a2d37e6f08b
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-29 11:54:11 +0200

    refactor(scrape_NIJZ_powerBI)(getDelivered)
    +getOldDelivered
    +scrapeDelivered
    +prepareDelivered

--- src/scrape_NIJZ_powerBI.js
+++ src/scrape_NIJZ_powerBI.js
@@ -100,67 +100,90 @@ const getTimestamp = async page => {
   return timestamp;
 };
 
-const getDelivered = async page => {
+const getOldDelivered = async () => {
   const oldData = await parsedDelivered();
+  const totalVacOld = oldData.reduce((acc, item) => {
+    const {
+      ['vaccination.pfizer.delivered']: pfizer,
+      'vaccination.moderna.delivered': moderna,
+      'vaccination.az.delivered': az,
+    } = item;
+    const num = pfizer || moderna || az;
+    acc = acc + +num;
+    return acc;
+  }, 0);
+  const oldDataLength = oldData.length;
+  return { oldDataLength, totalVacOld };
+};
 
+const scrapeDelivered = async (page, oldDataLength, COMPANIES_NUM) => {
   const scrollDown = await page.$(
     'div.tableEx > div:nth-child(4) > div:nth-child(2)'
   );
 
-  let lastDiv = null;
-  let length = 20; // max number of cells in column
-  // ! fix: if all columns have max number of cells -> infinite loop
-  while (length >= 20) {
-    // scroll down until last cell is visible
-    lastDiv = await page.$('div.bodyCells > div > div:last-child');
-    const columnDiv = await lastDiv.$('div');
-    const cellsDiv = await columnDiv.$$('div');
-    length = cellsDiv.length;
+  let cells = [];
+  let newDataLength = cells.length / COMPANIES_NUM;
+  let cont = true;
+  let totalVacs = 0;
 
+  while (newDataLength <= oldDataLength && cont) {
+    const vcBodies = await page.$$('.vcBody');
+    const textResolved = await Promise.all(
+      vcBodies.map(async item => {
+        const text = await item.$eval('div', item => item.innerText);
+        return text.startsWith('Datum\n') && text;
+      })
+    );
+    const allCells = textResolved
+      .filter(item => !!item)
+      .pop()
+      .split('\n');
+    totalVacs = +allCells.slice(-1).pop().replace('.', '');
+    cells = allCells.slice(4, allCells.length - 3);
+    newDataLength = cells.length / COMPANIES_NUM;
+    cont = newDataLength !== oldDataLength;
     await scrollDown.click();
   }
 
-  const columnsDiv = await page.$$('div.bodyCells > div > div');
-  const data = await Promise.all(
-    columnsDiv.map(async item => {
-      const columns = await item.$x('./div');
-      const [dateColumn, companyColumn, numColumn] = columns;
+  if (newDataLength === oldDataLength) {
+    return { noNewData: true };
+  }
+  return { cells, newDataLength, totalVacs };
+};
 
-      const dateCells = await dateColumn.$$('div');
-      const companyCells = await companyColumn.$$('div');
-      const numberCells = await numColumn.$$('div');
+const separateCellsByColumns = ({
+  cells,
+  newDataLength,
+  COMPANIES_NUM,
+  MAX_CELLS_NUM,
+}) => {
+  let dates = [];
+  let companies = [];
+  let numbers = [];
 
-      const datesPromises = dateCells.map(
-        async cell => await (await cell.getProperty('innerText')).jsonValue()
-      );
-      const companyPromises = companyCells.map(
-        async cell => await (await cell.getProperty('innerText')).jsonValue()
-      );
-      const numberPromises = numberCells.map(
-        async cell => await (await cell.getProperty('innerText')).jsonValue()
-      );
+  const dataLengthRatio = Math.floor(newDataLength / MAX_CELLS_NUM);
+  let index = 0;
+  let range = dataLengthRatio >= 1 ? MAX_CELLS_NUM : newDataLength;
+  while (dates.length < newDataLength) {
+    dates = [...dates, ...cells.slice(index, index + range)];
+    companies = [
+      ...companies,
+      ...cells.slice(index + range, index + range + range),
+    ];
+    numbers = [
+      ...numbers,
+      ...cells.slice(index + 2 * range, index + 2 * range + range),
+    ];
+    index += COMPANIES_NUM * range;
+    range =
+      cells.length - index >= 60
+        ? MAX_CELLS_NUM
+        : (cells.length - index) / COMPANIES_NUM;
+  }
+  return { dates, companies, numbers };
+};
 
-      const datesResolved = await Promise.all(datesPromises);
-      const companiesResolved = await Promise.all(companyPromises);
-      const numbersResolved = await Promise.all(numberPromises);
-      return {
-        dates: datesResolved,
-        companies: companiesResolved,
-        numbers: numbersResolved,
-      };
-    })
-  );
-
-  const reducedData = data.reduce(
-    (acc, { dates, companies, numbers }) => {
-      acc.dates = [...acc.dates, ...dates];
-      acc.companies = [...acc.companies, ...companies];
-      acc.numbers = [...acc.numbers, ...numbers];
-      return acc;
-    },
-    { dates: [], companies: [], numbers: [] }
-  );
-
+const prepareDelivered = ({ dates, companies, numbers }) => {
   const pfizer = 'vaccination.pfizer.delivered';
   const moderna = 'vaccination.moderna.delivered';
   const az = 'vaccination.az.delivered';
@@ -171,22 +194,55 @@ const getDelivered = async page => {
     ['Astra Zeneca']: az,
   };
 
-  const scrapedData = reducedData.dates.map((item, index) => {
+  return dates.map((item, index) => {
     const [day, month, year] = item.split('.');
     const date = new Date(Date.UTC(year, month - 1, day))
       .toISOString()
       .slice(0, 10);
-    const company = reducedData.companies[index];
-    const number = reducedData.numbers[index];
+    const company = companies[index];
+    const number = numbers[index].replace('.', '');
     const obj = { [dict[company]]: number };
     return { date, [pfizer]: '', [moderna]: '', [az]: '', ...obj };
   });
+};
 
-  if (scrapedData.length > oldData.length) {
-    return scrapedData;
+const getDelivered = async page => {
+  const COMPANIES_NUM = 3;
+  const MAX_CELLS_NUM = 20;
+  const { oldDataLength, totalVacOld } = await getOldDelivered();
+  const { cells, newDataLength, totalVacs, noNewData } = await scrapeDelivered(
+    page,
+    oldDataLength,
+    COMPANIES_NUM
+  );
+
+  if (noNewData) {
+    return null;
   }
 
-  return null;
+  const { dates, companies, numbers } = separateCellsByColumns({
+    cells,
+    newDataLength,
+    COMPANIES_NUM,
+    MAX_CELLS_NUM,
+  });
+
+  const total = numbers.reduce((acc, item) => {
+    const num = item.replace('.', '');
+    acc = acc + +num;
+    return acc;
+  }, 0);
+
+  const totalIsValid = total > totalVacOld && total === totalVacs;
+  totalIsValid && console.log(`Delivered total is valid, received: ${total}.`);
+
+  !totalVacOld &&
+    console.log(
+      `Delivered total is NOT valid, received: ${total}, expect: ${totalVacs}, greater than: ${totalVacOld}.`
+    );
+
+  const data = prepareDelivered({ dates, companies, numbers });
+  return data;
 };
 
 const scrap_NIJZ_powerBI = async () => {
@@ -206,6 +262,7 @@ const scrap_NIJZ_powerBI = async () => {
 
   await nextPage.click();
   await page.waitForSelector('.bodyCells');
+
   const delivered = await getDelivered(page);
 
   await browser.close();
