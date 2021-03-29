a67365bee1546d77024c03cfe0dd15e5bb186e77
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-03-29 20:24:18 +0200

    feat(scrape_NIJZ_powerBI): +vaccination

--- index.js
+++ index.js
@@ -10,12 +10,17 @@ const administeredBckPath = path.resolve(
   `csv/backup/vaccination-administered ${today}-${now}.csv`
 );
 
+const vaccinationBckPath = path.resolve(
+  dir,
+  `csv/backup/vaccination ${today}-${now}.csv`
+);
+
 const isDev = process.env.NODE_ENV === 'development';
 isDev && console.log(`running in ${process.env.NODE_ENV} mode!`);
 
 const start = async () => {
   const data = await scrape_NIJZ_powerBI();
-  const { timestamp, created, administered, delivered } = data;
+  const { timestamp, created, administered, delivered, vaccination } = data;
 
   console.log(`NIJZ date: ${new Date(timestamp.timestamp)}`);
   console.log(`Scrape date: ${new Date(created)}`);
@@ -27,6 +32,11 @@ const start = async () => {
 
   delivered !== null && writeCSV(delivered, 'delivered');
   delivered ?? console.log(`No new delivered data for ${today}!`);
+
+  vaccination !== null && isDev
+    ? writeCSV(vaccination, 'vaccination', vaccinationBckPath)
+    : writeCSV(vaccination, 'vaccination');
+  vaccination ?? console.log(`No new vaccination data for ${today}!`);
 };
 
 start();
