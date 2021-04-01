510440c14a471497bf13ac977798e8b3c3e68e72
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2021-04-01 12:02:32 +0200

    getAdministeredTable: attemp1 fail
    can not get all body cells. either to many or missing

--- src/getAdministeredTable/getAdministeredTable.js
+++ src/getAdministeredTable/getAdministeredTable.js
@@ -1,3 +1,13 @@
+import getTimestamp from './../getTimestamp/index.js';
+import nijzDashFetch from '../nijz-dash-fetch.js';
+
+const START_TS = Date.UTC(2020, 11, 27);
+const START_DATE = new Date(START_TS).toISOString().slice(0, 10);
+const START = {
+  TS: START_TS,
+  DATE: START_DATE,
+};
+
 const toNumber = string => {
   const newString = string.replace('.', '').replace(',', ''); // string.reaplaceAll -> node v15.0
   const dot = newString.indexOf('.');
@@ -8,7 +18,24 @@ const toNumber = string => {
   return toNumber(newString);
 };
 
+const getTimestamps = async page => {
+  const refreshed = await getTimestamp(page);
+  const today_ts = Date.now();
+  const today = {
+    ts: Date.now(),
+    date: new Date(today_ts).toISOString().slice(0, 10),
+  };
+
+  const days = (new Date(refreshed.date) - START.TS) / (24 * 60 * 60 * 1000);
+
+  return { today, refreshed, START, days };
+};
+
 export default async page => {
+  const timestamps = await getTimestamps(page);
+  const { dose1, dose2 } = await nijzDashFetch();
+  console.log({ timestamps, fetchedDoses: { dose1, dose2 } });
+
   const div = await page.$('div[title="Skupno število cepljenih oseb"]');
   await div.click({ button: 'right' });
   await page.waitForSelector('drop-down-list-item');
@@ -17,81 +44,42 @@ export default async page => {
   await page.waitForSelector('.rowHeaders');
   await page.waitForSelector('.bodyCells');
 
-  let rowHeaders;
   let cells = [];
   let bodyCells = null;
-  let c = null;
+  let cellsResolved = null;
+  let condition = true;
+  let counter = 1;
 
-  rowHeaders = await Promise.all(
-    await page.$$eval('.rowHeaders > div > div > div', headers =>
-      headers.map(header => header.innerText)
-    )
-  );
-  console.log(rowHeaders);
-  bodyCells = await page.$$('.bodyCells > div > div > div');
-  c = await Promise.all(
-    bodyCells.map(
-      async item =>
-        await item.$$eval('.pivotTableCellWrap', items =>
-          items.map(el => el.innerText)
-        )
-    )
-  );
-  cells.push(c.map(item => item.map(text => toNumber(text))));
+  while (counter < 40) {
+    bodyCells = await page.$$('.bodyCells > div > div > div');
+    cellsResolved = await Promise.all(
+      bodyCells.map(
+        async item =>
+          await item.$$eval('.pivotTableCellWrap', items =>
+            items.map(el => el.innerText)
+          )
+      )
+    );
+    cells.push(cellsResolved.map(item => item.map(text => toNumber(text))));
+    await page.$eval('.bodyCells', el => {
+      el.scrollBy(0, 20);
+    });
 
-  await page.$eval('.bodyCells', el => {
-    el.scrollTo(0, el.scrollHeight);
-  });
-  await page.waitForTimeout(500);
-  rowHeaders = await Promise.all(
-    await page.$$eval('.rowHeaders > div > div > div', headers =>
-      headers.map(header => header.innerText)
-    )
-  );
-  console.log(rowHeaders);
-  bodyCells = await page.$$('.bodyCells > div > div > div');
-  c = await Promise.all(
-    bodyCells.map(
-      async item =>
-        await item.$$eval('.pivotTableCellWrap', items =>
-          items.map(el => el.innerText)
-        )
-    )
-  );
-  cells.push(c.map(item => item.map(text => toNumber(text))));
+    const last = cells.slice(-1).pop();
+    const lastAdministered = last[0].slice(-1).pop();
+    const lastAdministered2nd = last[1].slice(-1).pop();
 
-  await page.$eval('.bodyCells', el => {
-    el.scrollTo(0, el.scrollHeight);
-  });
-  await page.waitForTimeout(500);
-
-  bodyCells = await page.$$('.bodyCells > div > div > div');
-  c = await Promise.all(
-    bodyCells.map(
-      async item =>
-        await item.$$eval('.pivotTableCellWrap', items =>
-          items.map(el => el.innerText)
-        )
-    )
-  );
-  cells.push(c.map(item => item.map(text => toNumber(text))));
-
-  await page.$eval('.bodyCells', el => {
-    el.scrollTo(0, el.scrollHeight);
-  });
-  await page.waitForTimeout(500);
-
-  bodyCells = await page.$$('.bodyCells > div > div > div');
-  c = await Promise.all(
-    bodyCells.map(
-      async item =>
-        await item.$$eval('.pivotTableCellWrap', items =>
-          items.map(el => el.innerText)
-        )
-    )
-  );
-  cells.push(c.map(item => item.map(text => toNumber(text))));
-
+    console.log({
+      cells,
+      length: cells.length,
+      lastAdministered,
+      lastAdministered2nd,
+      counter,
+      condition,
+    });
+    await page.waitForTimeout(1000);
+    counter++;
+  }
   const [administered, administered2nd] = cells.reduce(
     (acc, [first, second]) => {
       acc[0] = [...acc[0], ...first];
